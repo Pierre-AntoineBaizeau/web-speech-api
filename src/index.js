9f93d6baaa916c8f9a8ea197c92ad6f6b4b69064
Pierre Antoine Baizeau <pabaizeau@gmail.com>
2023-12-08 13:52:29 +0100

    add timeout

--- src/index.js
+++ src/index.js
@@ -2,47 +2,45 @@ import { findWordInPage } from "./utils.js";
 import { commandProcessor } from "./commandProcessor.js";
 
 export function webVoiceHub() {
-  const startListeningButton = document.getElementById("startListening");
+  // ... (le reste de votre code)
 
-  if (!startListeningButton) {
-    console.error("Element with ID 'startListening' not found.");
-    return;
-  }
-
-  startListeningButton.addEventListener("click", function () {
-    // Vérifiez si la reconnaissance vocale est supportée par le navigateur
-    if (
-      !("SpeechRecognition" in window) &&
-      !("webkitSpeechRecognition" in window)
-    ) {
-      alert(
-        "Your browser does not support speech recognition. Please try a modern browser like Chrome."
-      );
-      return;
-    }
-
-    const recognition = new (window.SpeechRecognition ||
-      window.webkitSpeechRecognition)();
-    recognition.interimResults = true;
-
-    recognition.onresult = (event) => {
-      let last = event.results.length - 1;
-      let text = event.results[last][0].transcript;
-
-      if (text.startsWith("find ")) {
-        let word = text.replace("find ", "").trim();
-        findWordInPage(word);
+  // Ajoutez un délai de 2 secondes avant d'initialiser la reconnaissance vocale
+  setTimeout(function () {
+    startListeningButton.addEventListener("click", function () {
+      // Vérifiez si la reconnaissance vocale est supportée par le navigateur
+      if (
+        !("SpeechRecognition" in window) &&
+        !("webkitSpeechRecognition" in window)
+      ) {
+        alert(
+          "Your browser does not support speech recognition. Please try a modern browser like Chrome."
+        );
         return;
       }
 
-      document.getElementById("output").innerText = text; // Affichez le texte dans la div
-      commandProcessor(text.toLowerCase()); // Envoyez le texte à notre processeur de commande
-    };
+      const recognition = new (window.SpeechRecognition ||
+        window.webkitSpeechRecognition)();
+      recognition.interimResults = true;
 
-    recognition.onerror = (event) => {
-      console.error("Error occurred in recognition: " + event.error);
-    };
+      recognition.onresult = (event) => {
+        let last = event.results.length - 1;
+        let text = event.results[last][0].transcript;
 
-    recognition.start(); // Commencez la reconnaissance
-  });
+        if (text.startsWith("find ")) {
+          let word = text.replace("find ", "").trim();
+          findWordInPage(word);
+          return;
+        }
+
+        document.getElementById("output").innerText = text; // Affichez le texte dans la div
+        commandProcessor(text.toLowerCase()); // Envoyez le texte à notre processeur de commande
+      };
+
+      recognition.onerror = (event) => {
+        console.error("Error occurred in recognition: " + event.error);
+      };
+
+      recognition.start(); // Commencez la reconnaissance
+    });
+  }, 2000); // Délai de 2 secondes
 }
